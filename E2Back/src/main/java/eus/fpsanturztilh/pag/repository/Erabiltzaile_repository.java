99b7073125eaabd9e0a5288965793cc103e4d97a
Cobas2288 <acobasv232@fpsanturtzilh.eus>
2025-02-11 12:05:04 +0100

    Erabiltzaileak

---
+++ E2Back/src/main/java/eus/fpsanturztilh/pag/repository/Erabiltzaile_repository.java
@@ -0,0 +1,7 @@
+package eus.fpsanturztilh.pag.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import eus.fpsanturztilh.pag.model.Erabiltzaileak;
+
+public interface Erabiltzaile_repository extends JpaRepository<Erabiltzaileak, String> {
+}
